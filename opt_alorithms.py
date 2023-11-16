f64665c8fa93e6aa203bc3e80bc1f9647bd5e034
Luiza Schneider Sampaio <98579033+luizasch2@users.noreply.github.com>
2023-11-16 19:22:06 -0300

    Create opt_alorithms.py

---
+++ opt_alorithms.py
@@ -0,0 +1,36 @@
+import numpy as np
+from randgraph import *
+from algor import *
+
+# função que calcula o custo do caminho
+def cost(route, matrix):
+    cost = 0
+    for i in range(len(route)):
+        cost += matrix[route[i-1]][route[i]] # soma o peso de cada aresta do caminho
+
+    return cost
+
+# função que aplica o algoritmo 2-opt dada a matriz de adjacencia e a rota a ser melhorada
+def two_opt(matrix, route):
+    best_route = route
+    improved = True
+    while improved:
+        improved = False
+        for i in range(1, len(route) - 2):
+           for j in range(i+1, len(route)):
+                new_route = route.copy() 
+                new_route[i:j] = route[j-1:i-1:-1] # Inverte a ordem dos elementos entre i e j
+                new_cost = cost(new_route, matrix) # Calcula o custo da nova rota
+                
+                if new_cost < cost(best_route, matrix): # Se o custo da nova rota for menor que o da rota atual, atualiza a rota
+                    best_route = new_route
+                    improved = True
+        route = best_route
+
+    return best_route
+
+
+
+
+
+
